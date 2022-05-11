ca67449f25e8e79925d038b0d7dab66c2f29f39d
Andreas Offenhaeuser <offenhaeuser@gmail.com>
2022-05-11 13:34:35 +0000

    add new columns to galaxy

--- userscript/teamview.js
+++ userscript/teamview.js
@@ -6,20 +6,15 @@ function addMenuButton () {
   const listEntry = document.createElement('li')
   const listLink = document.createElement('a')
   listLink.href = '#'
-  listLink.onclick = addChart
+  listLink.onclick = showSettings
   listLink.text = 'Teamview'
   listEntry.appendChild(listLink)
-  const statsEntry = document.getElementById('menu').children[14]
-  statsEntry.insertAdjacentElement('afterend', listEntry)
+  const ref = document.getElementById('menu').children[24]
+  ref.insertAdjacentElement('afterend', listEntry)
 }
 
-function addChart () {
-  const c = document.createElement('canvas')
-  c.id = 'charts'
-  c.height = 400
-  c.style = 'width: 100%; height: 400px;'
-  const content = document.getElementsByTagName('content')[0]
-  content.insertAdjacentElement('afterbegin', c)
+function showSettings () {
+  Array.from(document.querySelector('content').children).forEach(c => c.remove())
 }
 
 function getVisibleSystem () {
@@ -31,6 +26,8 @@ function getVisibleSystem () {
   const ROWS_HEADER = 2
   const ROWS_COUNT = 15
   const COLUMN_PLAYER = 5
+  const COLUMN_MOON = 3
+  const COLUMN_DEBRIS = 4
   const COLUMN_PLANETNAME = 2
   const rowsWithPlanets = Array.from(document.querySelector('content table.table569').querySelectorAll('tr')).slice(ROWS_HEADER, ROWS_HEADER + ROWS_COUNT)
   const entries = []
@@ -71,7 +68,49 @@ function requestPlayerData (names) {
   }))
 }
 
-function modifyPlayerText (playerData) {
+function addColumn (addCount = 1, titles = []) {
+  const rows = document.querySelector('content table.table569 tbody').children
+  for (let rowIx = 0; rowIx < rows.length; rowIx++) {
+    const tr = rows[rowIx]
+    const COLUMN_STYLE_REF = 2
+    let isSpan
+    switch (rowIx) {
+      case 0:
+        tr.children[0].setAttribute('colspan', 8 + addCount)
+        break
+      case 1:
+        for (let i = 0; i < addCount; i++) {
+          const e = document.createElement('th')
+          e.setAttribute('style', tr.children[COLUMN_STYLE_REF].getAttribute('style'))
+          console.log(tr.children[COLUMN_STYLE_REF].style)
+          let title = `Extra ${i + 1}`
+          if (titles.length === addCount) {
+            title = titles[i]
+          }
+          e.innerText = title
+          tr.appendChild(e)
+        }
+        break
+      case 17:
+      case 18:
+      case 19:
+      case 20:
+        isSpan = parseInt(tr.children[1].getAttribute('colspan'))
+        tr.children[1].setAttribute('colspan', isSpan + addCount)
+        break
+      case 21:
+        break
+      default:
+        for (let i = 0; i < addCount; i++) {
+          const e = document.createElement('td')
+          e.style = tr.children[COLUMN_STYLE_REF].style
+          tr.appendChild(e)
+        }
+    }
+  }
+}
+
+function modifyTable (playerData) {
   function cleanName (name) {
     return name.split('(')[0].trim()
   }
@@ -80,16 +119,25 @@ function modifyPlayerText (playerData) {
   const ROWS_HEADER = 2
   const ROWS_COUNT = 15
   const COLUMN_PLAYER = 5
+  const COLUMN_STATS = 8
   const rowsWithPlanets = Array.from(document.querySelector('content table.table569').querySelectorAll('tr')).slice(ROWS_HEADER, ROWS_HEADER + ROWS_COUNT)
   for (const row of rowsWithPlanets) {
     const cells = row.querySelectorAll('td')
     const playerName = cleanName(cells[COLUMN_PLAYER].innerText)
     if (playerName) {
+      const ingameRankStr = cells[COLUMN_PLAYER].querySelector('a')
+        .getAttribute('data-tooltip-content')
+        .split('pos. ')[1].split('<')[0]
+      const ingameRank = parseInt(ingameRankStr)
       const data = playerData.find(e => e.name === playerName)
       const s = document.createElement('span')
-      s.innerHTML = ` (${data.rank})`
+      s.innerHTML = ` ${ingameRank}`
       s.style = 'font-size: 80%; color: yellow;'
       cells[COLUMN_PLAYER].querySelector('a').appendChild(s)
+
+      const s2 = document.createElement('span')
+      s.innerText = `Fleet: ${data.pointsFleet}\nDefense: ${data.pointsDefense}`
+      cells[COLUMN_STATS].appendChild(s)
     }
   }
 }
@@ -97,11 +145,12 @@ function modifyPlayerText (playerData) {
 addMenuButton()
 
 if (window.location.search.includes('page=galaxy')) {
+  addColumn(2, ['Player Stats', 'Spio Info'])
   const data = getVisibleSystem()
   const players = data.map(e => e.playerName)
   const uniquePlayers = Array.from(new Set(players))
   requestPlayerData(uniquePlayers)
     .then(playerData => {
-      return modifyPlayerText(playerData)
+      return modifyTable(playerData)
     })
 }
