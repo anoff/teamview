c43897b19a56cf80be47a5289bcfe46a05fae6b4
Andreas Offenhaeuser <offenhaeuser@gmail.com>
2022-05-20 05:54:27 +0000

    update token script

--- createToken.js
+++ createToken.js
@@ -4,10 +4,10 @@ const logger = require('./lib/logger').child({ module: __filename })
 
 async function main () {
   const password = process.env.TOKEN_PASSWORD
-  const value = Token.generateValue()
+  const value = await Token.generateValue()
   const t = new Token('shared', value, Token.hashPassword(password))
   await t.save()
-  console.log(`Created token. Password=${password}, Token=${t.value}`)
+  console.log(`Created token. Password=${password}, Token=${value}, Object=${JSON.stringify(t.data)}`)
 }
 
 main().then(() => {
--- docker-update.sh
+++ docker-update.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
+set -e
 
 git pull
-docker-compose up --force-recreate --build -d
+docker compose up --force-recreate --build -d
 docker image prune -f
